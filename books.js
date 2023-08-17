fdc76ec84a10d4d3b79ebf146e72792e42ac0f2d
Djotajo <20823564+Djotajo@users.noreply.github.com>
2023-08-17 22:09:35 +0200

    Add lint and prettier formatting

--- books.js
+++ books.js
@@ -1,21 +1,19 @@
-console.log("Hi");
-
 const myLibrary = [];
 
 const content = document.querySelector(".content");
 
 function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.info = function (){
-        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
-    }
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.info = function () {
+    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
+  };
 }
 
 function addBookToLibrary(book) {
-  myLibrary.push(book)
+  myLibrary.push(book);
 }
 
 const book1 = new Book("The Hobbit", "J.R.R. Tolkien", 295, "not read yet");
@@ -27,34 +25,34 @@ addBookToLibrary(book2);
 console.log(myLibrary);
 
 function refresh() {
-    content.innerHTML = "";
-    myLibrary.forEach(book => {
-        let div = document.createElement('div');
-        div.classList.add("book");
-        let bookInfo = book.info().split(", ");
-        bookInfo.forEach(info => {
-            let para = document.createElement('p');
-            para.innerHTML = info;
-            div.appendChild(para);
-        });
-        let removeItem = document.createElement('button');
-        removeItem.innerHTML = "Remove";
-        div.appendChild(removeItem);
-        removeItem.addEventListener("click", function() {myLibrary.splice(myLibrary.indexOf(book), 1);
-            refresh();
-        console.log(myLibrary)});
-        content.appendChild(div);
-        console.log(div.innerHTML);
-        console.log(bookInfo);
+  content.innerHTML = "";
+  myLibrary.forEach((book) => {
+    const div = document.createElement("div");
+    div.classList.add("book");
+    const bookInfo = book.info().split(", ");
+    bookInfo.forEach((info) => {
+      const para = document.createElement("p");
+      para.innerHTML = info;
+      div.appendChild(para);
     });
+    const removeItem = document.createElement("button");
+    removeItem.innerHTML = "Remove";
+    div.appendChild(removeItem);
+    removeItem.addEventListener("click", () => {
+      myLibrary.splice(myLibrary.indexOf(book), 1);
+      refresh();
+      console.log(myLibrary);
+    });
+    content.appendChild(div);
+    console.log(div.innerHTML);
+    console.log(bookInfo);
+  });
 }
 
-// 
-// 
+//
+//
 const showButton = document.getElementById("showDialog");
 const addBook = document.getElementById("addBook");
-const outputBox = document.querySelector("output");
-const selectEl = addBook.querySelector("select");
 const confirmBtn = addBook.querySelector("#confirmBtn");
 
 const title = document.querySelector("#title");
@@ -65,37 +63,45 @@ const bookStatus = document.querySelector("#status");
 let createnew;
 
 showButton.addEventListener("click", () => {
-    addBook.showModal();
+  addBook.showModal();
 });
 
-// "Cancel" button closes the dialog without submitting because of [formmethod="dialog"], triggering a close event.
-const closeBtn = document.querySelector("#closeBtn"); 
+const closeBtn = document.querySelector("#closeBtn");
 
-closeBtn.addEventListener("click", function() {
-    addBook.close();
-    myForm.reset();
+closeBtn.addEventListener("click", () => {
+  addBook.close();
+  myForm.reset();
 });
 
-// Prevent the "confirm" button from the default behavior of submitting the form, and close the dialog with the `close()` method, which triggers the "close" event.
 confirmBtn.addEventListener("click", (event) => {
-    let formCheck = document.getElementById('myForm').checkValidity();
-    if (!formCheck) {
-        document.getElementById('myForm').reportValidity();
-    } else{
+  const formCheck = document.getElementById("myForm").checkValidity();
+  if (!formCheck) {
+    document.getElementById("myForm").reportValidity();
+  } else {
     event.preventDefault(); // We don't want to submit this fake form
     createnew = [title.value, author.value, pages.value, bookStatus.value];
-    let newbook = new Book(createnew[0], createnew[1], createnew[2], createnew[3]);
-    newbook.number = myLibrary.findIndex((book) => book.title ==newbook.title);
+    const newbook = new Book(
+      createnew[0],
+      createnew[1],
+      createnew[2],
+      createnew[3]
+    );
+    newbook.number = myLibrary.findIndex(
+      (book) => book.title === newbook.title
+    );
     addBookToLibrary(newbook);
     console.log(createnew);
     console.log(newbook.number);
     refresh();
-    addBook.close();}
-    myForm.reset();
+    addBook.close();
+  }
+  myForm.reset();
 });
 
 refresh();
 
-document.getElementById('myForm').checkValidity();
+document.getElementById("myForm").checkValidity();
 
 console.log(myLibrary);
+
+const opa = document.querySelector("#title");
