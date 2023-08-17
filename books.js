9b4bf68615be6172a7b5c9ced195c06da960aae3
Djotajo <20823564+Djotajo@users.noreply.github.com>
2023-08-17 20:28:10 +0200

    Implement add book and remove book functionality

--- books.js
+++ books.js
@@ -5,10 +5,10 @@ const myLibrary = [];
 const content = document.querySelector(".content");
 
 function Book(title, author, pages, read) {
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.read = read
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
     this.info = function (){
         return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
     }
@@ -37,16 +37,17 @@ function refresh() {
             para.innerHTML = info;
             div.appendChild(para);
         });
+        let removeItem = document.createElement('button');
+        removeItem.innerHTML = "Remove";
+        div.appendChild(removeItem);
+        removeItem.addEventListener("click", function() {myLibrary.splice(myLibrary.indexOf(book), 1);
+            refresh();
+        console.log(myLibrary)});
         content.appendChild(div);
         console.log(div.innerHTML);
         console.log(bookInfo);
     });
 }
-
-
-// let div = document.createElement('div');
-// div.classList.add("book");
-// content.appendChild(div);
 
 // 
 // 
@@ -68,23 +69,33 @@ showButton.addEventListener("click", () => {
 });
 
 // "Cancel" button closes the dialog without submitting because of [formmethod="dialog"], triggering a close event.
-addBook.addEventListener("close", (e) => {
-  outputBox.value =
-  addBook.returnValue === "default"
-      ? "No return value."
-      : `ReturnValue: ${addBook.returnValue}.`; // Have to check for "default" rather than empty string
+const closeBtn = document.querySelector("#closeBtn"); 
+
+closeBtn.addEventListener("click", function() {
+    addBook.close();
+    myForm.reset();
 });
 
 // Prevent the "confirm" button from the default behavior of submitting the form, and close the dialog with the `close()` method, which triggers the "close" event.
 confirmBtn.addEventListener("click", (event) => {
-  event.preventDefault(); // We don't want to submit this fake form
-  createnew = [title.value, author.value, pages.value, bookStatus.value];
-  let newbook = new Book(createnew[0], createnew[1], createnew[2], createnew[3]);
-  addBookToLibrary(newbook);
-  console.log(createnew);
-  console.log(myLibrary);
-  refresh();
-  addBook.close();
+    let formCheck = document.getElementById('myForm').checkValidity();
+    if (!formCheck) {
+        document.getElementById('myForm').reportValidity();
+    } else{
+    event.preventDefault(); // We don't want to submit this fake form
+    createnew = [title.value, author.value, pages.value, bookStatus.value];
+    let newbook = new Book(createnew[0], createnew[1], createnew[2], createnew[3]);
+    newbook.number = myLibrary.findIndex((book) => book.title ==newbook.title);
+    addBookToLibrary(newbook);
+    console.log(createnew);
+    console.log(newbook.number);
+    refresh();
+    addBook.close();}
+    myForm.reset();
 });
 
 refresh();
+
+document.getElementById('myForm').checkValidity();
+
+console.log(myLibrary);
